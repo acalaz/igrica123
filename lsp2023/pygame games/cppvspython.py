a424bf22822d2f332d4ceb1e2345efecc82321d7
acalaz <140891580+acalaz@users.noreply.github.com>
2023-08-01 13:21:15 +0200

    Update cppvspython.py

--- lsp2023/pygame games/cppvspython.py
+++ lsp2023/pygame games/cppvspython.py
@@ -1,16 +1,17 @@
-# Example file showing a basic pygame "game loop"
-import pygame
+import pygame, sys
 bc = 0
 bp = 0
 global yc
 global yp
 # pygame setup
 xc = 1100
-yc = 560
+yc = 555
 xp = 0
-yp = 560
+yp = 555
 bcx = xc
 bpx = xp
+bcy = yc
+bpy = yp
 col = False
 global shootingp
 global shootingc
@@ -19,7 +20,14 @@ shootingp = False
 jumpc = False
 jumpp = False
 vel = 5
-
+cddy = 2
+acc = 1
+dxc = 0
+dyc = 0
+pbcx = xc
+pbcy = yc
+pbpx = xp
+pbpy = yp
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
@@ -42,8 +50,7 @@ pyt = pyt4.get_rect
 cbeam = cbeam1.get_rect
 pbeam = pbeam1.get_rect
 key = pygame.key.get_pressed()
-
-
+dyyc = 0.5 # Brzina gravitacije 0.01
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -53,34 +60,43 @@ while running:
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
-
+    key = pygame.key.get_pressed()
     # RENDER YOUR GAME HERE
-    if bcx == xp+200:
+    if bcx == xp+200 and bcy == yp:
         bp+=1
-    if bpx == xc-200:
+    if bpx == xc-200 and bpy == yp:
         bc+=1
     if bp == 5:
         pygame.quit()
     if bc == 5:
         pygame.quit()
-    key = pygame.key.get_pressed()
+
+    
     if key[pygame.K_LEFT] and xc>0:
         xc-=10
     if key[pygame.K_RIGHT] and xc<1280-150:
         xc+=10
-    #if key[pygame.K_UP] and yc > vel:  # Same principles apply for the y coordinate
-    #    jumpc = True
-    #    yc -= vel
-    #    screen.blit(cp3, (xc, yc))
-    #if jumpc == True:
-    #    yc += vel
+    if key[pygame.K_UP]:
+        dyc = -10 # Kolko visoko skace
+    
+    xc += dxc
+    if yc+dyc<720-166:
+        yc += dyc
+    dyc += dyyc
+    
+    if dyc > 3:  # Max brzina skakanja 0.8
+        dyc = 3
+    
     if key[pygame.K_a] and xp>0:
         xp-=4
     if key[pygame.K_d] and xp<1280-160:
         xp+=4
+    
     if key[pygame.K_m]:
+        pbcx = xc
         shootingc = True
         bcx=xc
+        
         screen.blit(cbeam1, (bcx-30, yc+50))
     if shootingc == True:
         bcx-=5
@@ -90,6 +106,7 @@ while running:
     if key[pygame.K_q]:
         shootingp = True
         bpx=xp
+        
         screen.blit(pbeam1, (bpx+150, yc+50))
     if shootingc == True:
         bpx+=3
@@ -100,6 +117,7 @@ while running:
     screen.blit(pyt4, (xp, yp))
     pygame.display.update()
 
-        
+    pbcx = xc
+    pbpx = xp
     clock.tick(60)  # limits FPS to 60
 pygame.quit()
