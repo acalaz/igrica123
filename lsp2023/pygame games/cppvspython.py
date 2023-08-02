89fc63b12206825ef53f91b5f3c523aa3bf6b136
acalaz <140891580+acalaz@users.noreply.github.com>
2023-08-02 09:48:26 +0200

    Update cppvspython.py

--- lsp2023/pygame games/cppvspython.py
+++ lsp2023/pygame games/cppvspython.py
@@ -17,14 +17,13 @@ global shootingp
 global shootingc
 shootingc = False
 shootingp = False
-jumpc = False
-jumpp = False
-vel = 5
-cddy = 2
-acc = 1
+#\/ varijable za skakanje
 dxc = 0
 dyc = 0
-pbcx = xc
+dxp = 0
+dyp = 0
+#/\ varijable za skakanje
+pbcx = xc #pomocne promenljive za pucanje da ne bi metak leteo kada neko skoci
 pbcy = yc
 pbpx = xp
 pbpy = yp
@@ -45,12 +44,14 @@ cp3 = pygame.transform.scale(cp1, (width1/3, height1/3))
 pyt4 = pygame.transform.scale(pyt3, (width/2, height/2))
 cbeam1 = pygame.transform.scale(lbeam, (width/5.5, height/5.5))
 pbeam1 = pygame.transform.rotate(cbeam1, 180) 
-cpp = cp3.get_rect
-pyt = pyt4.get_rect
-cbeam = cbeam1.get_rect
-pbeam = pbeam1.get_rect
-key = pygame.key.get_pressed()
+cpp = cp3.get_rect()     #ovo je za collision koje sad probam
+pyt = pyt4.get_rect()
+cbeam = cbeam1.get_rect()
+pbeam = pbeam1.get_rect()
+                        #ovo je za collision koje sad probam
+key = pygame.key.get_pressed() #za tastatu
 dyyc = 0.5 # Brzina gravitacije 0.01
+dyyp = 0.5 #
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -62,9 +63,10 @@ while running:
     screen.fill("white")
     key = pygame.key.get_pressed()
     # RENDER YOUR GAME HERE
-    if bcx == xp+200 and bcy == yp:
-        bp+=1
-    if bpx == xc-200 and bpy == yp:
+    #if cpp.collidedict(pbeam)  #####nije gotovo. Ovo je da li je pythonov metak pogodio cpp
+    if bcx == xp+200 and bcy == yp:  #od 64 do 73 linije proverava da li je metak pogdio u cpp odnosno python
+        bp+=1                        #ne radi bas kako treba tako da sad probam sa collidedict
+    if bpx == xc-200 and bpy == yc:
         bc+=1
     if bp == 5:
         pygame.quit()
@@ -74,37 +76,47 @@ while running:
         exit()
 
     
-    if key[pygame.K_LEFT] and xc>0:
+    if key[pygame.K_LEFT] and xc>0:   #pomeranje cpp
         xc-=10
     if key[pygame.K_RIGHT] and xc<1280-150:
         xc+=10
-    if key[pygame.K_UP]:
+    if key[pygame.K_UP]:             #skakanje
         dyc = -10 # Kolko visoko skace
     
     xc += dxc
-    if yc+dyc<720-166:
+    if yc+dyc<=720-165:
         yc += dyc
     dyc += dyyc
     
     if dyc > 3:  # Max brzina skakanja 0.8
         dyc = 3
     
-    if key[pygame.K_a] and xp>0:
+    if key[pygame.K_a] and xp>0:    #pomeranje pythona
         xp-=4
     if key[pygame.K_d] and xp<1280-160:
         xp+=4
-    
+    if key[pygame.K_w]:
+        dyp = -10
+    xp += dxp
+    if yp+dyp<=720-165:
+        yp += dyp
+    dyp+=dyyp
+    if dyp > 3:
+        dyp = 3
+    # /\ sve ovo je skakanje pythona
     if key[pygame.K_m]:
         pbcy = yc
         shootingc = True
         bcx=xc
-        
+    #pucanje cpp
         screen.blit(cbeam1, (bcx-30, pbcy+50))
     if shootingc == True:
         bcx-=5
         screen.blit(cbeam1, (bcx-30, pbcy+50))
     else:
         shootingc = False
+    #pucanje cpp
+    #\/ pucanje pythona
     if key[pygame.K_q]:
         pbcy = yp
         shootingp = True
@@ -116,11 +128,13 @@ while running:
         screen.blit(pbeam1, (bpx+150, pbpy+50))
     else:
         shootingc = False
-    screen.blit(cp3, (xc, yc))
+    #/\ pucanje pythona
+    screen.blit(cp3, (xc, yc))#prikazianje
     screen.blit(pyt4, (xp, yp))
     pygame.display.update()
 
     pbcx = xc
     pbpx = xp
     clock.tick(60)  # limits FPS to 60
+    print(yp, yc) #ovo sam ispisivao y kordinate zbog nekog testa
 pygame.quit()
